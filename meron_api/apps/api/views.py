585dece0848c45b8bcfa2c8b1685b2126b94a8a7
Jesaja Everling <jeverling@gmail.com>
2018-08-29 02:52:50 +0200

    Only return HTML if client requests it

--- meron_api/apps/api/views.py
+++ meron_api/apps/api/views.py
@@ -5,7 +5,7 @@ from meron_api.apps.api.serializers import (
     FaceDetectionInputSerializer,
     FaceDetectionOutputSerializer,
 )
-from rest_framework.renderers import StaticHTMLRenderer
+from rest_framework.renderers import JSONRenderer, StaticHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
@@ -15,9 +15,15 @@ class FaceDetectionResultView(APIView):
     """Accept POST requests with image, call face detection function and return rendered results."""
 
     def get_renderers(self):
-        """Instantiate and return renderers that this view can use."""
-        if self.get_renderer_context()["request"].method == "GET":
-            return [StaticHTMLRenderer()]
+        """Override renderers if we are dealing with a GET request.
+
+        DEFAULT_RENDERER_CLASSES are JSONRenderer and ReadOnlyBrowsableAPIRenderer.
+        If a browser opens the index page, we want to display the README.md as
+        HTML instead of the API browser
+        """
+        request = self.get_renderer_context()["request"]
+        if request.method == "GET":
+            return [JSONRenderer(), StaticHTMLRenderer()]
         return [renderer() for renderer in self.renderer_classes]
 
     def post(self, request):
@@ -35,21 +41,32 @@ class FaceDetectionResultView(APIView):
         return Response(input_serializer.errors, status=HTTP_400_BAD_REQUEST)
 
     def get(self, request):
-        """Display a rendered version of README.md."""
-        md = open("README.md").read()
-        rendered_html = markdown.markdown(md)
-        html = """
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>MERON API</title>
-</head>
-<body>
-{}
-</body>
-</html>
-""".format(
-            rendered_html
+        """Display a rendered version of README.md for browsers.
+
+        Other clients (e.g. curl) should see a message about usage.
+        """
+        if request.accepted_renderer.format == "html":
+            md = open("README.md").read()
+            rendered_html = markdown.markdown(md)
+            html = """<!DOCTYPE html>
+                      <html>
+                      <head>
+                      <meta charset="UTF-8">
+                      <title>MERON API</title>
+                      </head>
+                      <body>
+                      {}
+                      </body>
+                      </html>
+                   """.format(
+                rendered_html
+            )
+            return Response(html)
+
+        msg = (
+            "Please make a POST request with an image. Other paramters are "
+            "'score' (Boolean, default True), 'classification' (Boolean, "
+            "default True), 'age' (Integer, age in months), 'gender' (String"
+            ", 'm' or 'f')"
         )
-        return Response(html)
+        return Response({"message": msg})
